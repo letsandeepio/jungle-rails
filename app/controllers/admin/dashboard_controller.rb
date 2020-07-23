b8d6706e1e4c2bf05aabf06650426b7ffff3618f
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-07-23 15:03:54 +0000

    add products & category stats to the admin dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,6 +5,6 @@ class Admin::DashboardController < ApplicationController
 
   def show
     @product_count = Product.all.count
-    @category_count= Category.all.count
-end
+    @category_count = Category.all.count
+  end
 end
