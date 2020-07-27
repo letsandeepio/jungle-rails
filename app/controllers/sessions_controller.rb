ff1ea63cdfaa6670f02039057dbdce8d3098e10e
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-07-27 20:33:48 +0000

    change user to instance variable

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.authenticate_with_credentials(params[:email], params[:password])
-    if user
-      session[:user_id] = user.id
+    @user = User.authenticate_with_credentials(params[:email], params[:password])
+    if @user
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       redirect_to '/login'
