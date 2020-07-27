51dbc4e69a5a0ef3cdebf626316359401c70f9a7
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-07-27 19:57:06 +0000

    adds validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@
 
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :name, :password, :password_confirmation, :email, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 10 }
+  validates :password, confirmation: true
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    user if user&.authenticate(password)
+  end
 end
