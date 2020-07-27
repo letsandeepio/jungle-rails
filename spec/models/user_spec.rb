0660c839d7b9dd41c9892eaadd15ecc2724d023f
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-07-27 20:34:11 +0000

    adds testing conditions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'it is valid if it has a name, email, password, password confirmation'
+
+    it 'its invalid without name'
+
+    it 'its invalid without email'
+
+    it 'its invalid if password & confirmation do not match'
+
+    it 'its invalid if emails already exists'
+
+    it 'its invalid if password length is less than 10 characters'
+  end
+
+  describe '.authenticate_with_credentials' do
+    # examples for this class method here
+  end
 end
